2405c4414b76d02b78e8260185c822789d642ed3
Rylan Badgett <Rybad614@gmail.com>
2023-10-16 13:04:40 -0400

    finished

---
+++ app.js
@@ -0,0 +1,32 @@
+/*
+ES5 Global Constants
+
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+
+ES2015 Global Constants
+--Write an ES2015 Version--
+
+const PI = 3.14;
+PI = 42; // referenceError:
+
+Quiz
+
+- What is the difference between var and let?
+
+*var is function-scoped, let is scoped to code block.
+*let cannot redeclare while var can.
+
+- What is the difference between var and const?
+*const cannot effect window methods while var can.
+*Hoisting, var allows access before declaring variables.
+
+- What is the difference between let and const?
+*const cannot reassign
+
+- What is hoisting?
+*The placement of a variable to the top of the scope
+they consist of.
+
+*/
